9bb089ade6c84ba3dbf2e45774b5e2d93bd1e777
RackelRodrigues <rackelrodrigues247@gmail.com>
2025-06-25 14:22:36 -0300

    add improvements to styles, data, and images

--- src/pages/Destination/styles.ts
+++ src/pages/Destination/styles.ts
@@ -1,20 +1,16 @@
 import styled from "styled-components";
 
-import tablet from "../../assets/images/destination/background-destination-desktop.jpg";
-import desktop from "../../assets/images/destination/background-destination-tablet.jpg";
-import mobile from "../../assets/images/destination/background-destination-mobile.jpg";
-
 export const Background = styled.div`
   width: 100%;
   height: 100vh;
-  background-image: url(${desktop});
+  background-image: url("/images/destination/background-destination-desktop.jpg");
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
   padding: 2rem 0;
 
   @media screen and (max-width: 1024px) and (min-width: 610px) {
-    background-image: url(${tablet});
+    background-image: url("images/destination/background-destination-tablet.jpg");
     background-position: center;
     background-size: cover;
     overflow-x: hidden;
@@ -22,7 +18,7 @@ export const Background = styled.div`
   }
 
   @media screen and (max-width: 609px) {
-    background-image: url(${mobile});
+    background-image: url("images/destination/background-destination-mobile.jpg");
     background-position: center;
     background-size: cover;
     padding-bottom: 2rem;
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,25 +1,22 @@
 import styled from "styled-components";
-import tablet from "../../assets/images/home/background-home-tablet.jpg";
-import desktop from "../../assets/images/home/background-home-desktop.jpg";
-import mobile from "../../assets/images/home/background-home-mobile.jpg";
 
 export const Background = styled.div`
   width: 100%;
   height: 100vh;
-  background-image: url(${desktop});
+  background-image: url("images/home/background-home-desktop.jpg");
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
   padding: 2rem 0;
 
   @media screen and (max-width: 1024px) and (min-width: 610px) {
-    background-image: url(${tablet});
+    background-image: url("images/home/background-home-tablet.jpg");
     background-position: center;
     background-size: cover;
   }
 
   @media sreen and (max-width: 609px) {
-    background-image: url(${mobile});
+    background-image: url("images/home/background-home-mobile.jpg");
     background-position: center;
     background-size: cover;
     height: 100%;
